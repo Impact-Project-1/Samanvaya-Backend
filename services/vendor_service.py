8a7b2a2930c9e18653f2ad7687496162a89cf398
rohitrn_ <rohitrajeev05@gmail.com>
2026-05-26 11:25:54 +0530

    modernized vendor cruds

--- services/vendor_service.py
+++ services/vendor_service.py
@@ -1,41 +1,46 @@
 from repo import vendor_repo
+from schemas.vendor_schema import VendorCreate
 
 
-def create_vendor(vendor_data):
+def create_vendor(vendor_data: VendorCreate) -> dict:
 
-    data = vendor_data.dict()
+    data = vendor_data.model_dump()
 
     data["verified"] = False
     data["rating_avg"] = 0
     data["rating_count"] = 0
-    
-    print(data) 
 
     return vendor_repo.create_vendor(data)
 
 
-def get_all_vendors():
+def get_all_vendors() -> list[dict]:
 
     return vendor_repo.get_all_vendors()
 
 
-def get_vendor_by_id(vendor_id):
-    return vendor_repo.get_vendor_by_id(vendor_id)
+def get_vendor_by_id(vendor_id: str) -> dict:
+    return vendor_repo.get_vendor_by_id(vendor_id).data[0]
 
 
-def update_vendor(vendor_id, vendor_data):
+def update_vendor(vendor_id: str, vendor_data: VendorCreate) -> dict:
 
-    data = vendor_data.dict()
+    data = vendor_data.model_dump()
 
     return vendor_repo.update_vendor(vendor_id, data)
 
 
-def delete_vendor(vendor_id):
+def delete_vendor(vendor_id: str) -> dict:
     return vendor_repo.delete_vendor(vendor_id)
 
-def search_by_name(name):
+def search_by_name(name: str | None = None) -> list[dict]:
     return vendor_repo.search_vendor_by_name(name)
 
-def filter_vendors(city: str = None, state: str = None, category: str = None, min_price: float = None, max_price: float = None, rating: float = None, sort_by: str = None):
+def filter_vendors(city: str | None = None,
+                   state: str | None = None,
+                   category: str | None = None,
+                   min_price: float | None = None,
+                   max_price: float | None = None,
+                   rating: float | None = None,
+                   sort_by: str | None = None) -> list[dict]:
     return vendor_repo.filter_vendors(city,state,category,min_price,max_price,rating,sort_by)
 
