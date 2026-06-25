8dc8fa50a3cd0080054e97cd864b950d1fcb4e93
Devika <dileepdevika3@gmail.com>
2026-06-25 19:41:25 +0530

    update CORS settings to include frontend URL

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
+        "https://samanvaya-frontend.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
